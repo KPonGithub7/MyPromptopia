4f1928dda153b374a6cdf6f78799d8e542979cba
Kunal Patel <kunalpatel88843@gmail.com>
2024-01-22 19:29:28 +0530

    Animation Added

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,63 +1,75 @@
+"use client";
 import Link from "next/link";
+import Cursor from "./Cursor";
+import Magnetic from "./Magnetic";
+import { useState } from "react";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+    const [hover, setHover] = useState(false);
     return (
-        <section className="w-full max-w-full flex-col flex-start">
-            <h1 className="head_text font-satoshi text-left">
-                <span className="blue_gradient">{type} Post</span>
-            </h1>
-            <p className="desc text-left max-w-md">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
-                accusantium, voluptatum non cum placeat similique corporis
-                laboriosam libero error natus, unde vero facilis illo blanditiis
-                quia iure magnam est soluta!
-            </p>
-            <form
-                onSubmit={handleSubmit}
-                className="w-full max-w-2xl mt-10 flex flex-col gap-7 glasmorphism "
-            >
-                <label>
-                    <span className="font-satoshi font-semibold text-base text-gray-700">
-                        Prompt
-                    </span>
-                    <textarea
-                        value={post.prompt}
-                        onChange={(e) =>
-                            setPost({ ...post, prompt: e.target.value })
-                        }
-                        placeholder="Write your promp here..."
-                        required
-                        className="form_textarea"
-                    ></textarea>
-                </label>
-                <label>
-                    <span className="font-satoshi font-semibold text-base text-gray-700">
-                        Tag
-                    </span>
-                    <input
-                        value={post.tags}
-                        onChange={(e) =>
-                            setPost({ ...post, tags: e.target.value })
-                        }
-                        placeholder="#tags"
-                        required
-                        className="form_input"
-                    ></input>
-                </label>
-                <div className="flex flex-end gap-4 mx-3 mb-5 ">
-                    <Link href="/" className="text-gray-500 text-sm">
-                        <span>Cancel</span>
-                    </Link>
-                    <button
-                        className="rounded-full bg-primary-orange px-5 py-1.5 text-sm text-white"
-                        type="submit"
-                        disabled={submitting}
-                    >
-                        {type}
-                    </button>
-                </div>
-            </form>
-        </section>
+        <>
+            <Cursor hover={hover} />
+            <section className="w-full max-w-full flex-col flex-start">
+                <h1 className="head_text font-satoshi text-left">
+                    <span className="blue_gradient">{type} Post</span>
+                </h1>
+                <p className="desc text-left max-w-md">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+                    accusantium, voluptatum non cum placeat similique corporis
+                    laboriosam libero error natus, unde vero facilis illo
+                    blanditiis quia iure magnam est soluta!
+                </p>
+                <form
+                    onSubmit={handleSubmit}
+                    className="w-full max-w-2xl mt-10 flex flex-col gap-7 glasmorphism "
+                >
+                    <label>
+                        <span className="font-satoshi font-semibold text-base text-gray-700">
+                            Prompt
+                        </span>
+                        <textarea
+                            value={post.prompt}
+                            onChange={(e) =>
+                                setPost({ ...post, prompt: e.target.value })
+                            }
+                            placeholder="Write your promp here..."
+                            required
+                            className="form_textarea"
+                        ></textarea>
+                    </label>
+                    <label>
+                        <span className="font-satoshi font-semibold text-base text-gray-700">
+                            Tag
+                        </span>
+                        <input
+                            value={post.tags}
+                            onChange={(e) =>
+                                setPost({ ...post, tags: e.target.value })
+                            }
+                            placeholder="#tags"
+                            required
+                            className="form_input"
+                        ></input>
+                    </label>
+                    <div className="flex flex-end gap-4 mx-3 mb-5 ">
+                        <Link href="/" className="text-gray-500 text-sm">
+                            <span>Cancel</span>
+                        </Link>
+                        <Magnetic>
+                            <button
+                                className="rounded-full bg-primary-orange px-5 py-1.5 text-sm text-white z-0 "
+                                type="submit"
+                                disabled={submitting}
+                                onMouseOver={() => setHover(true)}
+                                onMouseLeave={() => setHover(false)}
+                            >
+                                {type}
+                            </button>
+                        </Magnetic>
+                    </div>
+                </form>
+            </section>
+        </>
     );
 };
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -4,8 +4,16 @@ import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
+import Magnetic from "./Magnetic";
+import Cursor from "./Cursor";
 
-const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+const PromptCard = ({
+    post,
+    handleTagClick,
+    handleEdit,
+    handleDelete,
+    hovering,
+}) => {
     const { data: session } = useSession();
     const pathName = usePathname();
     const router = useRouter();
@@ -25,72 +33,81 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         );
     };
     return (
-        <div className="prompt_card">
-            <div className="flex items-center justify-between gap-5">
-                <div
-                    className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
-                    onClick={handleProfileClick}
-                >
-                    <Image
-                        src={post.creator.image}
-                        alt="user_image"
-                        width={40}
-                        height={40}
-                        className="rounded-full object-contain"
-                    />
-                    <div className="flex flex-col">
-                        <h3 className="font-satoshi font-semibold text-gray-900">
-                            {post.creator.username}
-                        </h3>
-                        <p className="font-inter text-sm text-gray-500">
-                            {post.creator.email}
-                        </p>
+        <>
+            {/* <Cursor /> */}
+            <div className="prompt_card">
+                <div className="flex items-center justify-between gap-5">
+                    <div
+                        className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+                        onClick={handleProfileClick}
+                    >
+                        <Image
+                            src={post.creator.image}
+                            alt="user_image"
+                            width={40}
+                            height={40}
+                            className="rounded-full object-contain"
+                        />
+                        <div className="flex flex-col">
+                            <h3 className="font-satoshi font-semibold text-gray-900">
+                                {post.creator.username}
+                            </h3>
+                            <p className="font-inter text-sm text-gray-500">
+                                {post.creator.email}
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <Magnetic>
+                            <Image
+                                src={
+                                    copied === post.prompt
+                                        ? "/assets/icons/tick.svg"
+                                        : "/assets/icons/copy.svg"
+                                }
+                                width={18}
+                                height={18}
+                                alt="copy"
+                                className="cursor-pointer "
+                                onClick={handleCopy}
+                                onMouseOver={() => hovering(true)}
+                                onMouseLeave={() => hovering(false)}
+                            />
+                        </Magnetic>
                     </div>
                 </div>
                 <div>
-                    <Image
-                        src={
-                            copied === post.prompt
-                                ? "/assets/icons/tick.svg"
-                                : "/assets/icons/copy.svg"
+                    <p className="my-4 font-satoshi text-gray-700 text-sm">
+                        {post.prompt}
+                    </p>
+                    <p
+                        className="font-inter blue_gradient text-sm cursor-pointer"
+                        onClick={() =>
+                            handleTagClick && handleTagClick(post.tags)
                         }
-                        width={12}
-                        height={12}
-                        alt="copy"
-                        className="cursor-pointer"
-                        onClick={handleCopy}
-                    />
+                    >
+                        {post.tags}
+                    </p>
                 </div>
+                {session?.user.id === post.creator._id &&
+                    pathName === "/profile" && (
+                        <div className="mt-5 flex-center gap-4 border-t border-gray-500 pt-3">
+                            <p
+                                className="blue_gradient cursor-pointer font-inter text-sm "
+                                onClick={handleEdit}
+                            >
+                                Edit
+                            </p>
+                            <p
+                                className="orange_gradient cursor-pointer font-inter text-sm "
+                                onClick={handleDelete}
+                            >
+                                Delete
+                            </p>
+                        </div>
+                    )}
             </div>
-            <div>
-                <p className="my-4 font-satoshi text-gray-700 text-sm">
-                    {post.prompt}
-                </p>
-                <p
-                    className="font-inter blue_gradient text-sm cursor-pointer"
-                    onClick={() => handleTagClick && handleTagClick(post.tags)}
-                >
-                    {post.tags}
-                </p>
-            </div>
-            {session?.user.id === post.creator._id &&
-                pathName === "/profile" && (
-                    <div className="mt-5 flex-center gap-4 border-t border-gray-500 pt-3">
-                        <p
-                            className="blue_gradient cursor-pointer font-inter text-sm "
-                            onClick={handleEdit}
-                        >
-                            Edit
-                        </p>
-                        <p
-                            className="orange_gradient cursor-pointer font-inter text-sm "
-                            onClick={handleDelete}
-                        >
-                            Delete
-                        </p>
-                    </div>
-                )}
-        </div>
+        </>
     );
 };
 
